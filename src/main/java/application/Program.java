1dcc55985f7677bc98c856c7632ceac955a41410
HenriqueSenaDev <acopsenadeveloper@gmail.com>
2024-10-12 18:00:16 -0300

    feat: placing pieces on the board

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,9 @@
 package application;
 
-import boardgame.Board;
 import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-//        Board board = new Board(8, 8);
-
         ChessMatch match = new ChessMatch();
         UI.printBoard(match.getPieces());
     }
--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
         this.board = new Board(8, 8);
+        this.initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
@@ -19,5 +23,11 @@ public class ChessMatch {
         }
 
         return mat;
+    }
+
+    private void initialSetup() {
+        this.board.placePiece(new Rook(this.board, Color.WHITE), new Position(2, 1));
+        this.board.placePiece(new King(this.board, Color.BLACK), new Position(0, 4));
+        this.board.placePiece(new King(this.board, Color.WHITE), new Position(7, 4));
     }
 }
